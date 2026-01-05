ef2fc61778cc09f22fc5b7e53083e7bed1a2d0ab
Briana Garcia <briana_garcia_@outlook.com>
2026-01-04 23:00:56 -0800

    Format

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const JoinPage = () => {
-  return (
-    <div>JoinPage</div>
-  )
-}
+  return <div>JoinPage</div>;
+};
 
-export default JoinPage
+export default JoinPage;
