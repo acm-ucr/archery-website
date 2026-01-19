d68469791b50a1e5210be292fb9cd92dead06e38
kevinjylin <kevinjylin@gmail.com>
2026-01-19 11:31:07 -0800

    fix formatting

--- src/app/example.tsx
+++ src/app/example.tsx
@@ -2,11 +2,11 @@ const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
     <div className="bg-archery-blue-500 mx-auto w-2/3 rounded-xl p-8">
       <div className="grid grid-rows-3 justify-items-center gap-10">
-        <div className="bg-archery-pink-100 w-[55%] p-5 text-center text-2xl text-white">
+        <div className="bg-archery-pink-100 w-1/2 p-5 text-center text-2xl text-white">
           {props.text1}
         </div>
 
-        <div className="grid w-[55%] grid-cols-2 gap-5">
+        <div className="grid w-1/2 grid-cols-2 gap-5">
           <div className="bg-archery-blue-100 w-full rounded-full p-5 text-center text-2xl">
             {props.text2}
           </div>
@@ -16,7 +16,7 @@ const Example = (props: { text1: string; text2: string; text3: string }) => {
           </div>
         </div>
 
-        <div className="bg-archery-yellow-100 w-[55%] p-5 text-center text-2xl">
+        <div className="bg-archery-yellow-100 w-1/2 p-5 text-center text-2xl">
           {props.text3}
         </div>
       </div>
