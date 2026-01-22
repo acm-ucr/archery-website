3bdb2b7a260b9ea13b86f95c4fef8ac315a69499
akaFM <orca55556@gmail.com>
2026-01-22 15:07:44 -0800

    completed flexbox lesson

--- src/app/example.tsx
+++ src/app/example.tsx
@@ -1,17 +1,24 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-archery-blue-500 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-archery-blue-100 w-1/2 p-5 text-left">
-        {props.text1}
-      </div>
+    <div className="bg-archery-blue-500 flex w-2/3 items-center justify-center rounded-3xl py-12">
+      <div className="flex w-1/2 flex-col items-center gap-8">
+        <div className="bg-archery-pink-100 flex w-full items-center justify-center rounded-md py-5 text-center text-3xl font-semibold text-white">
+          {props.text1}
+        </div>
 
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-archery-blue-100 w-full p-5 text-left">
-          {props.text2}
+        <div className="flex w-full flex-row items-center justify-center gap-2">
+          <div className="bg-archery-blue-100 flex flex-1 items-center justify-center rounded-full py-5 text-center text-2xl font-semibold">
+            {props.text2}
+          </div>
+          <div className="bg-archery-blue-100 flex flex-1 items-center justify-center rounded-full py-5 text-center text-2xl font-semibold">
+            {props.text2}
+          </div>
+        </div>
+
+        <div className="bg-archery-yellow-100 flex w-full items-center justify-center rounded-md py-5 text-center text-2xl font-semibold">
+          {props.text3}
         </div>
       </div>
-
-      <div className="bg-archery-blue-100 w-1/2 p-5">{props.text3}</div>
     </div>
   );
 };
