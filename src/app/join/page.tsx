de841faddedc8d162bf850523a448ab5102f2f17
Yilin Liu <yilinliu741@gmail.com>
2026-01-24 01:23:46 -0800

    Formatted images according to figma

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header";
 import FAQ from "@/components/join/faq";
+import GalleryGrid from "@/components/join/galleryGrid";
 
 const JoinPage = () => {
   return (
     <>
       <Header title="Join" subTitle="Archery @UCR" />
+      <GalleryGrid />
       <FAQ />
     </>
   );
---
+++ src/components/join/galleryGrid.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import topLeft from "@/public/gallery_topleft.webp";
+import midLeft from "@/public/gallery_midleft.webp";
+import bottomLeft from "@/public/gallery_bottomleft.webp";
+import topRight from "@/public/gallery_topright.webp";
+import midRight from "@/public/gallery_midright.webp";
+import bottomRight from "@/public/gallery_bottomright.webp";
+
+const GalleryGrid = () => {
+  return (
+    <div className="relative mt-40 ml-20 flex w-1/2 flex-col gap-2">
+      <div className="flex gap-2">
+        <Image
+          src={topLeft}
+          alt="Picture of Archery Club Members"
+          className="object-cover"
+        />
+        <Image
+          src={topRight}
+          alt="Picture of Archery Club Members"
+          className="object-cover"
+        />
+      </div>
+      <div className="flex gap-2">
+        <Image
+          src={midLeft}
+          alt="Picture of Archery Club Members"
+          className="object-cover"
+        />
+        <Image
+          src={midRight}
+          alt="Picture of Archery Club Members"
+          className="object-cover"
+        />
+      </div>
+      <div className="flex gap-2">
+        <Image
+          src={bottomLeft}
+          alt="Picture of Archery Club Members"
+          className="object-cover"
+        />
+        <Image
+          src={bottomRight}
+          alt="Picture of Archery Club Members"
+          className="ml-2 object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GalleryGrid;
