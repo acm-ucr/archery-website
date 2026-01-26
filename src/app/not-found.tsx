562a07b58031eb127334e706b73e99c42aa32c83
Joshua Bumatay <joshua@bumatay.net>
2026-01-26 14:48:52 -0800

    Removed arrows from link tag

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,17 +5,20 @@ import RightArrow from "@/public/404-right-arrow.svg";
 
 const NotFound = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-yellow-100 via-blue-100 to-blue-300">
+    <div className="flex h-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-r from-yellow-100 via-blue-100 to-blue-300">
       <div className="text-archery-blue-500 mb-3 text-7xl font-bold">
         {"404"}
       </div>
-      <Link href="/" className="flex cursor-pointer items-center gap-0">
+      <div className="flex items-center">
         <Image src={LeftArrow} alt="Left end of arrow" />
-        <div className="border-archery-blue-500 text-archery-blue-500 -mx-1 rounded-full border-5 bg-white px-12 py-2 text-xl font-semibold">
+        <Link
+          href="/"
+          className="border-archery-blue-500 text-archery-blue-500 -mx-1 rounded-full border-5 bg-white px-12 py-2 text-xl font-semibold"
+        >
           {"Return Back Home"}
-        </div>
+        </Link>
         <Image src={RightArrow} alt="Right end of arrow" />
-      </Link>
+      </div>
     </div>
   );
 };
