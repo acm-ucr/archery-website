76a798afefa2961b6da973a9870b5a9a3290a4f3
Joshua Bumatay <joshua@bumatay.net>
2026-01-25 16:39:50 -0800

    made requested changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
+import LeftArrow from "@/public/404-left-arrow.svg";
+import RightArrow from "@/public/404-right-arrow.svg";
 
 const NotFound = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-r from-yellow-100 via-blue-100 to-blue-300">
-      <div className="mb-3 text-7xl font-bold text-[#0A3386]">{"404"}</div>
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-yellow-100 via-blue-100 to-blue-300">
+      <div className="text-archery-blue-500 mb-3 text-7xl font-bold">
+        {"404"}
+      </div>
       <Link href="/" className="flex cursor-pointer items-center gap-0">
-        <Image src="404-left-arrow.svg" alt="" width={81} height={28} />
-        <div className="-mx-1 rounded-full border-[5px] border-[#0A3386] bg-white px-12 py-2 text-xl font-semibold text-[#0A3386]">
+        <Image src={LeftArrow} alt="Left end of arrow" />
+        <div className="border-archery-blue-500 text-archery-blue-500 -mx-1 rounded-full border-5 bg-white px-12 py-2 text-xl font-semibold">
           {"Return Back Home"}
         </div>
-        <Image src="404-right-arrow.svg" alt="" width={82} height={21} />
+        <Image src={RightArrow} alt="Right end of arrow" />
       </Link>
     </div>
   );
