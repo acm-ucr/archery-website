57980c0d4bab33600dd185c19c8c2c3b8accdd06
Yilin Liu <yilinliu741@gmail.com>
2026-01-26 12:12:38 -0800

    Removed w-1/2 from parent flex container

--- src/components/join/galleryGrid.tsx
+++ src/components/join/galleryGrid.tsx
@@ -8,7 +8,7 @@ import bottomRight from "@/public/gallery_bottomright.webp";
 
 const GalleryGrid = () => {
   return (
-    <div className="relative flex w-1/2 flex-col items-center gap-2">
+    <div className="relative flex flex-col items-center gap-2">
       <div className="flex gap-2">
         <Image
           src={topLeft}
