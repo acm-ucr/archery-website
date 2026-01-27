0c41362586624c6245734d31d1af5255b6890fdc
Tarunvir <gtarunvir@gmail.com>
2026-01-27 14:34:59 -0800

    fixed issues: color, footer tag, sizing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,44 +1,45 @@
 import Link from "next/link";
 import Image from "next/image";
 import { footerLinks } from "@/data/footerLinks";
+import ArcheryLogo from "@/public/archery-ucr-logo.svg";
+import FooterArrow from "@/public/footer-arrow.svg";
 
 const Footer = () => {
   return (
-    <footer className="border-t-3 border-blue-900 bg-white">
-      <div className="relative space-y-3 py-5">
-        <p className="mt-5 pl-9 text-lg text-blue-900"> Contact Us :</p>
-        {footerLinks.map(({ icon: Icon, url, text }, index) => (
-          <Link
-            key={index}
-            href={url}
-            className="flex items-center gap-2 pl-9 text-gray-600"
-          >
-            <Icon className="h-4 w-4 fill-blue-900" />
-            {text}
-          </Link>
-        ))}
-        <div className="absolute top-1/2 right-30 -translate-y-1/2">
-          <Image
-            src="acherylogo.png"
-            alt="Archery Club Logo"
-            width={20}
-            height={20}
-          />
+    <div className="border-archery-blue-600 border-t-3">
+      <div className="flex items-center justify-between p-2">
+        <div className="space-y-3 py-5">
+          <p className="text-archery-blue-600 mt-5 pl-9 text-lg">
+            Contact Us :
+          </p>
+          {footerLinks.map(({ icon: Icon, url, text }, index) => (
+            <Link
+              key={index}
+              href={url}
+              target="_blank"
+              className="text-archery-grey-300 flex items-center gap-2 pl-9"
+            >
+              <Icon className="fill-archery-blue-600 h-6 w-6" />
+              {text}
+            </Link>
+          ))}
+        </div>
+        <div>
+          <Image src={ArcheryLogo} alt="Archery Club Logo" className="pr-10" />
         </div>
       </div>
-
-      <div className="top--4 absolute right-10">
+      <div className="border-archery-blue-600 relative border-t-3 py-3">
         <Image
-          src="footerarrow.svg"
+          src={FooterArrow}
           alt="Footer Arrow"
-          width={30}
-          height={10}
+          className="absolute top-0 right-10 -translate-y-1/2"
         />
+
+        <p className="text-archery-grey-200 mt-5 text-center text-xs">
+          Made with 💘
+        </p>
       </div>
-      <p className="mt-5 border-t-3 border-blue-900 py-3 text-center text-xs text-gray-500">
-        Made with 💘
-      </p>
-    </footer>
+    </div>
   );
 };
 
