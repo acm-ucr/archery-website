a0667aa290fe126cba45d04cb4b3fded2afe0951
Shikha Suralikal <shikhasuralikal@Shikhas-MacBook-Pro.local>
2026-01-29 17:57:59 -0800

    Converted Images to webp and added small changes

--- src/components/about/aboutLanding.tsx
+++ src/components/about/aboutLanding.tsx
@@ -1,6 +1,8 @@
-import Target from "@/public/Group 16.svg";
-import Arrow from "@/public/Group 18.svg";
-import DownButton from "@/public/Down Button.svg";
+import Target from "@/public/AboutPageTarget.webp";
+import TargetCenter from "@/public/MiddleOfTarget.webp";
+import Arrow from "@/public/AboutPageArrow.webp";
+import ArrowTip from "@/public/AboutPageArrowTip.webp";
+import DownButton from "@/public/Down Button.webp";
 import Image from "next/image";
 
 const AboutLanding = () => {
@@ -16,13 +18,28 @@ const AboutLanding = () => {
           archery!
         </p>
         <p className="pt-32 text-center font-extralight">learn more about us</p>
-        <Image src={DownButton} alt="Down Button" className="w-auto pl-60" />
+        <Image src={DownButton} alt="Down Button" className="ml-60 w-4" />
       </div>
       <Image src={Target} alt="Image of a target" className="z-10 w-1/2" />
       <Image
+        src={TargetCenter}
+        alt="Part of Center of Target"
+        className="absolute inset-x-324 inset-y-97 z-10 w-3"
+      />
+      <Image
+        src={TargetCenter}
+        alt="Part of Center of Target"
+        className="absolute inset-x-324 inset-y-97 z-10 w-3 scale-x-[-1] -rotate-6"
+      />
+      <Image
         src={Arrow}
         alt="Image of an arrow"
-        className="absolute inset-x-267 inset-y-87 z-20 w-1/6"
+        className="absolute inset-x-266 inset-y-85 z-20 w-1/6"
+      />
+      <Image
+        src={ArrowTip}
+        alt="Arrow Tip"
+        className="absolute inset-x-325 inset-y-97.5 z-10 w-1.5"
       />
       <Image
         src={Arrow}
@@ -30,9 +47,19 @@ const AboutLanding = () => {
         className="absolute inset-x-268 inset-y-90 z-20 w-1/6"
       />
       <Image
+        src={ArrowTip}
+        alt="Arrow Tip"
+        className="absolute inset-x-327 inset-y-102.5 z-10 w-1.5"
+      />
+      <Image
         src={Arrow}
         alt="Image of an arrow"
         className="absolute inset-x-255 inset-y-92 z-20 w-1/6"
+      />
+      <Image
+        src={ArrowTip}
+        alt="Arrow Tip"
+        className="absolute inset-x-314 inset-y-104.5 z-10 w-1.5"
       />
 
       <div className="bg-archery-blue-100 absolute top-1/5 z-0 h-1/4 w-1/5 rounded-full blur-3xl filter"></div>
