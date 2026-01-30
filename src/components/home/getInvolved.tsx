cb8feba635f51f83b389ee2013442b7a73ff5679
djechi <djechipare@gmail.com>
2026-01-29 18:14:28 -0800

    Changed color fractions

--- src/components/home/getInvolved.tsx
+++ src/components/home/getInvolved.tsx
@@ -7,17 +7,17 @@ const GetInvolved = () => {
     <div className="flex flex-row items-center justify-center gap-x-20 p-8">
       <InvolvedCard
         borderColor="border-archery-blue-300"
-        bgColor="bg-archery-blue-100"
-        iconBG="bg-archery-blue-200"
+        bgColor="bg-archery-blue-300/10"
+        iconBG="bg-archery-blue-300/30"
         title="NO experience required!"
         description="complete beginners are welcome! We’ll teach you everything from basic stance to competitive skills."
         icon={BlueArcher}
       />
 
       <InvolvedCard
-        borderColor="border-archery-yellow-300"
-        bgColor="bg-archery-yellow-100"
-        iconBG="bg-archery-yellow-200"
+        borderColor="border-archery-yellow-200"
+        bgColor="bg-archery-yellow-200/20"
+        iconBG="bg-archery-yellow-200/40"
         title="NO equipment required!"
         description="we provide all the basic equipment -- bows, arrows, safety gear -- for you to get started!"
         icon={YellowBow}
