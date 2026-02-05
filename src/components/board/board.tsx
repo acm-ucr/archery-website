85a6ebbfd587a670ef8814ee1a0255757ef0f59d
Jacob <pokemine007craft@gmail.com>
2026-02-05 10:05:10 -0800

    Add board member grid with placeholder information

---
+++ src/components/board/board.tsx
@@ -0,0 +1,24 @@
+import Card from "@/components/board/card";
+import Icon from "@/public/boardMemberIcon.png";
+
+const boardMembers = [
+    { src: Icon, name: "First Last Name", position: "position" },
+    { src: Icon, name: "First Last Name", position: "position" },
+    { src: Icon, name: "First Last Name", position: "position" },
+    { src: Icon, name: "First Last Name", position: "position" },
+    { src: Icon, name: "First Last Name", position: "position" },
+    { src: Icon, name: "First Last Name", position: "position" },
+    { src: Icon, name: "First Last Name", position: "position" }
+];
+
+const Board = () => {
+    return (
+        <div className="flex flex-wrap items-center justify-center gap-10">
+            {boardMembers.map((member, index) => (
+                <Card key={index} src={member.src} name={member.name} position={member.position} />
+            ))}
+        </div>
+    );
+}
+
+export default Board;
