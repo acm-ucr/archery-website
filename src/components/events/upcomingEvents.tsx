26ef32eff7d2f4b14bf8ae17af0ad85fd23781ed
Yilin Liu <yilinliu741@gmail.com>
2026-02-06 11:37:22 -0800

    Added icons and formatted the component

--- src/components/events/upcomingEvents.tsx
+++ src/components/events/upcomingEvents.tsx
@@ -1,32 +1,50 @@
+import { IoTimeOutline } from "react-icons/io5";
+import { MdOutlineLocationOn } from "react-icons/md";
+
 interface Events {
-    day : string;
-    date : string;
-    title : string;
-    startTime : string;
-    endTime : string;
-    location : string;
-    description : string;       
-
+  day: string;
+  date: string;
+  title: string;
+  startTime: string;
+  endTime: string;
+  location: string;
+  description: string;
 }
-const UpcomingEvents = ({day, date, title, startTime, endTime, location, description}: Events) => {
+const UpcomingEvents = ({
+  day,
+  date,
+  title,
+  startTime,
+  endTime,
+  location,
+  description,
+}: Events) => {
   return (
-    <div className="flex gap-4 ">
-        <div className="flex flex-col items-center justify-center">
-          <p className="text-archery-blue-500 text-bold text-4xl">{day}</p>
-          <p className="text-archery-blue-500 text-bold text-4xl">{date}</p>
-        </div>
-        <div className="flex flex-col">
-          <div className="h-5 w-full"/>
-          <p className="text-archery-blue-500 text-2xl font-bold">{title}</p>
-          <div className="flex gap-2">
-            <p className="text-archery-blue-500 font-bold text-lg">{startTime} - {endTime}</p>
-            <p className="text-archery-blue-500 font-bold text-lg">{location}</p>  
+    <div className="flex w-3/4 gap-8 justify-self-center p-5">
+      <div className="flex flex-col items-center justify-center gap-2 px-8">
+        <p className="text-archery-blue-500 text-5xl font-bold">{day}</p>
+        <p className="text-archery-blue-500 text-5xl font-bold">{date}</p>
+      </div>
+      <div className="mx-8 flex flex-col gap-6">
+        <div className="bg-archery-blue-500 h-[0.3vh] w-full" />
+        <div className="ml-8 flex w-5/6 flex-col gap-2">
+          <p className="text-archery-blue-500 text-4xl font-bold">{title}</p>
+          <div className="my-2 flex items-center gap-1">
+            <IoTimeOutline className="text-archery-blue-500 text-2xl" />
+            <p className="text-archery-blue-500 mr-4 ml-1 text-lg font-bold">
+              {startTime} - {endTime}
+            </p>
+            <MdOutlineLocationOn className="text-archery-blue-500 text-2xl" />
+            <p className="text-archery-blue-500 text-lg font-bold">
+              {location}
+            </p>
           </div>
-          <p className="text-archery-blue-500 text-lg">{description}</p>
-          <div className="h-5 w-full"/>
+          <p className="text-archery-blue-500 text-lg/6">{description}</p>
         </div>
+        <div className="bg-archery-blue-500 h-[0.3vh] w-full" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UpcomingEvents
+export default UpcomingEvents;
