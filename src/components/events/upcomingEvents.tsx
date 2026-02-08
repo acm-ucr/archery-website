d6f8bd69e9961c875067f85d99194e0c75b7c180
Yilin Liu <yilinliu741@gmail.com>
2026-02-07 18:59:25 -0800

    Added top and bottom border and other minor fixes

--- src/components/events/upcomingEvents.tsx
+++ src/components/events/upcomingEvents.tsx
@@ -21,12 +21,11 @@ const UpcomingEvents = ({
 }: Events) => {
   return (
     <div className="flex w-3/4 gap-8 justify-self-center p-5">
-      <div className="flex flex-col items-center justify-center gap-2 px-8">
-        <p className="text-archery-blue-500 text-5xl font-bold">{day}</p>
-        <p className="text-archery-blue-500 text-5xl font-bold">{date}</p>
+      <div className="text-archery-blue-500 flex flex-col items-center justify-center gap-2 px-8 text-5xl font-bold">
+        <p>{day}</p>
+        <p>{date}</p>
       </div>
-      <div className="mx-8 flex flex-col gap-6">
-        <div className="bg-archery-blue-500 h-[0.3vh] w-full" />
+      <div className="border-archery-blue-500 mx-8 flex flex-col gap-6 border-y-2 py-6">
         <div className="ml-8 flex w-5/6 flex-col gap-2">
           <p className="text-archery-blue-500 text-4xl font-bold">{title}</p>
           <div className="my-2 flex items-center gap-1">
@@ -41,7 +40,6 @@ const UpcomingEvents = ({
           </div>
           <p className="text-archery-blue-500 text-lg/6">{description}</p>
         </div>
-        <div className="bg-archery-blue-500 h-[0.3vh] w-full" />
       </div>
     </div>
   );
