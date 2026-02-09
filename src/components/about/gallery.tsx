7b0594046680e10f2683f4c573811f7bdcb59373
Tarunvir <gtarunvir@gmail.com>
2026-02-09 10:21:24 -0800

    merge branch 'main' into tarunvir/gallercard

--- src/components/about/gallery.tsx
+++ src/components/about/gallery.tsx
@@ -12,8 +12,8 @@ const Gallery = () => {
         fill
         className="object-cover contrast-50"
       />
-      <div className="absolute inset-0 bg-gradient-to-b from-black/5 to-black/15"></div>
-      <div className="relative container mx-auto px-8 md:px-16">
+      <div className="absolute inset-0 bg-gradient-to-b from-white via-white/10" />
+      <div className="container mx-auto px-8 md:px-16">
         <div className="grid grid-cols-1 gap-4 p-8 md:grid-cols-2 md:gap-12">
           {galleryItems.map(({ title, image }, index) => (
             <GalleryCard key={index} title={title} image={image} />
