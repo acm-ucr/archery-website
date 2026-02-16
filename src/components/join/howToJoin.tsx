c4f7fb048fd7da7d3d456ec00e901b58c40227a3
kevinjylin <kevinjylin@gmail.com>
2026-02-15 17:01:38 -0800

    got image and icon to show up

--- src/components/join/howToJoin.tsx
+++ src/components/join/howToJoin.tsx
@@ -1,14 +1,18 @@
 import { LuTarget } from "react-icons/lu";
-import Image, {/public/ClubSports.png} from "next/image";
-
+import Image from "next/image";
+import ClubSports from "@/public/ClubSports.png";
 
 const HowToJoin = () => {
-    return (
-        <div className="text-archery-grey-300 flex flex-col items-center justify-center">
-            <div className="mb-5 w-1/2 max-w-xs">
-                <Image src={ClubSports} alt="Club Sports Logo" className="w-full" />
-            </div>
-            </div>
-    )
+  return (
+    <div className="text-archery-grey-300 flex flex-col items-center justify-center">
+      <div className="mb-5 w-1/2 max-w-xs">
+        <Image src={ClubSports} alt="Club Sports Logo" className="w-full" />
+      </div>
+      <div className="flex items-center justify-center">
+        <LuTarget className="text-4xl" aria-label="Target" />
+        <div className=""></div>
+      </div>
+    </div>
+  );
 };
 export default HowToJoin;
