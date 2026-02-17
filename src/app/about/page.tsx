2a0a291bb32c8aab118cc770a9565160b878ee5d
Tarunvir <gtarunvir@gmail.com>
2026-02-16 20:56:18 -0800

    About landing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
+import AboutLanding from "@/components/about/aboutLanding";
 import Gallery from "@/components/about/gallery";
 const AboutPage = () => {
   return (
     <>
+      <AboutLanding />
       <Gallery />
     </>
   );
