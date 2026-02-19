2135f7e81beea23f13bf8545b682dab14832f816
kevinjylin <kevinjylin@gmail.com>
2026-02-19 12:54:43 -0800

    created first line and 3rd line

--- src/components/join/howToJoin.tsx
+++ src/components/join/howToJoin.tsx
@@ -6,11 +6,18 @@ const HowToJoin = () => {
   return (
     <div className="text-archery-grey-300 flex flex-col items-center justify-center">
       <div className="mb-5 w-1/2 max-w-xs">
+        <div className="flex items-center justify-center">
+          <LuTarget className="text-2xl" aria-label="Target" />
+          <div className="text-archery-grey-300 whitespace-pre-line">
+            Complete Your UCR DoSportsEasy waiver. Archery --> Registration
+            --> fill out information
+          </div>
+        </div>
         <Image src={ClubSports} alt="Club Sports Logo" className="w-full" />
       </div>
       <div className="flex items-center justify-center">
-        <LuTarget className="text-4xl" aria-label="Target" />
-        <div className=""></div>
+        <LuTarget className="text-2xl" aria-label="Target" />
+        <div className="text-archery-grey-300 ">Test</div>
       </div>
     </div>
   );
