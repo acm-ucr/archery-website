07abf822bf997d1b4d1720b8a684947aaac1b2f0
kevinjylin <kevinjylin@gmail.com>
2026-02-21 18:27:24 -0800

    added correct pictures

--- src/components/join/howToJoin.tsx
+++ src/components/join/howToJoin.tsx
@@ -1,23 +1,48 @@
-import { LuTarget } from "react-icons/lu";
+import Target from "@/public/Target_Weld.svg";
 import Image from "next/image";
 import ClubSports from "@/public/ClubSports.png";
+import InnerClubSports from "@/public/InnerClubSports.png";
 
 const HowToJoin = () => {
   return (
-    <div className="text-archery-grey-300 flex flex-col items-center justify-center">
+    <div className="text-archery-grey-200 flex flex-col items-center justify-center">
       <div className="mb-5 w-1/2 max-w-xs">
-        <div className="flex items-center justify-center">
-          <LuTarget className="text-2xl" aria-label="Target" />
-          <div className="text-archery-grey-300 whitespace-pre-line">
-            Complete Your UCR DoSportsEasy waiver. Archery --> Registration
-            --> fill out information
+        <div className="flex items-start">
+          <Image src={Target} alt="Target" className="mx-8 w-12 object-cover" />
+          <div className="whitespace-pre-line">
+            Complete Your UCR DoSportsEasy waiver. Archery → Registration → fill
+            out information
           </div>
         </div>
-        <Image src={ClubSports} alt="Club Sports Logo" className="w-full" />
+        <div className="relative aspect-[4/3] w-full max-w-md overflow-hidden">
+  {/* Outer image */}
+  <Image
+    src={ClubSports}
+    alt="Outer Club Sports Logo"
+    fill
+    priority
+    className="object-contain"
+  />
+
+  {/* White bottom band */}
+  <div className="absolute bottom-0 left-[2%] right-[2%] h-[65%] bg-white z-10" />
+
+  {/* Inner logo centered in the white band */}
+  <div className="absolute inset-x-0 bottom-0 h-[28%] z-20 flex items-center justify-center">
+    <div className="relative h-[70%] w-[70%] max-w-[180px]">
+      <Image
+        src={InnerClubSports}
+        alt="Inner Club Sports Logo"
+        fill
+        className="object-contain"
+      />
+    </div>
+  </div>
+</div>
       </div>
       <div className="flex items-center justify-center">
-        <LuTarget className="text-2xl" aria-label="Target" />
-        <div className="text-archery-grey-300 ">Test</div>
+        <Image src={Target} alt="Target" className="mx-8 w-12 object-cover" />
+        <div className="text-archery-grey-200">Test</div>
       </div>
     </div>
   );
