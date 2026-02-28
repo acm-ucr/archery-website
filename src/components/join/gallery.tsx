4706e1dedef4b02176ca0fb05fd35cadd96ea3e4
Yilin Liu <yilinliu741@gmail.com>
2026-02-27 22:51:30 -0800

    Added responsiveness for lg and xl

--- src/components/join/gallery.tsx
+++ src/components/join/gallery.tsx
@@ -7,58 +7,65 @@ import { Feeinfo } from "@/data/feeinfo";
 
 const Gallery = () => {
   return (
-    <div className="gap-6 md:gap-0 mt-8 md:mt-20 xl:mt-40 flex flex-col items-center md:ml-8 justify-self-center md:grid w-5/6 md:w-9/10 md:grid-cols-2">
-      <div className=" justify-self-center flex flex-col md:ml-8 items-center xl:gap-2 text-center md:col-start-2 md:row-start-1 w-9/10 md:w-3/5 ">
-        <p className="text-archery-grey-400 text-xl xl:mb-6 xl:text-4xl xl:leading-11 font-bold">
+    <div className="mt-8 flex w-5/6 flex-col items-center gap-6 justify-self-center md:mt-20 md:ml-8 md:grid md:w-9/10 md:grid-cols-2 md:items-start md:gap-0 lg:mt-30 xl:mt-40">
+      <div className="mt-6 flex w-9/10 flex-col items-center justify-self-center text-center md:col-start-2 md:row-start-1 md:ml-8 md:w-3/5 lg:w-5/9 xl:w-7/12">
+        <p className="text-archery-grey-400 text-xl font-bold lg:text-2xl xl:mb-6 xl:text-4xl xl:leading-11">
           Everything you need to know about joining!
         </p>
-        <div className="flex items-center md:flex-col gap-1 mt-4 xl:mt-6">
-          <div className="w-[2.5vh] xl:w-1/12"><Image src={Warning} alt="Warning" className="object-cover" /></div>
-          <p className="text-xs xl:text-xl text-red-700">members must be UCR students</p>
+        <div className="mt-4 flex items-center gap-1 md:mt-8 md:flex-col lg:mt-10 xl:mt-12">
+          <div className="w-4 lg:w-6 xl:w-8">
+            <Image src={Warning} alt="Warning" className="object-cover" />
+          </div>
+          <p className="text-xs text-red-700 lg:text-base xl:text-xl">
+            members must be UCR students
+          </p>
         </div>
       </div>
-      <div className=" md:row-span-2 md:row-start-1 ">
+      <div className="md:row-span-2 md:row-start-1">
         <GalleryGrid />
       </div>
-      <div className="md:w-8/9 xl:w-3/4 flex-col items-center  justify-self-center text-center md:col-start-2 md:row-span-1 md:row-start-2">
-        <div className="xl:my-8 md:my-4 flex items-center justify-center gap-2 xl:gap-4">
+      <div className="flex-col items-center justify-self-center text-center md:col-start-2 md:row-span-1 md:row-start-2 md:w-8/9 lg:w-4/5 xl:w-3/4">
+        <div className="mb-2 flex items-center justify-center gap-2 md:mb-4 lg:mb-6 xl:mb-8 xl:gap-4">
           <Image
             src={SalaryMan}
             alt="Membership Fee"
-            className="justify-self-center w-[5vh] md:w-[6vh] lg:w-[7vh] object-cover"
+            className="mb-2 w-8 justify-self-center object-cover md:w-10 lg:w-12 xl:w-16"
           />
-          <p className="text-archery-grey-400 xl:text-4xl text-xl font-bold">
+          <p className="text-archery-grey-400 text-xl font-bold lg:text-2xl xl:text-4xl">
             Membership Fee
           </p>
         </div>
-        <div className=" flex w-7/8 ml-8 flex-col gap-4 my-4 justify-self-center md:justify-self-end">
+        <div className="mb-4 ml-8 flex w-7/8 flex-col gap-4 justify-self-center md:justify-self-end xl:gap-8">
           <div className="flex items-start">
             <Image
               src={Target}
               alt="Bullet Point 1"
-              className="ml-5 mr-4 md:ml-6 md:mr-5 xl:mx-7 xl:mr-6 w-[2.5vh] object-cover"
+              className="mr-4 ml-5 w-4 object-cover md:mr-5 md:ml-6 lg:mx-7 lg:mr-6 lg:w-6 xl:w-8"
             />
-            <p className="text-archery-grey-400 xl:mt-1 md:w-2/3 text-start text-xs xl:text-xl">
+            <p className="text-archery-grey-400 text-start text-xs md:w-2/3 lg:text-base xl:text-xl">
               {" "}
               <span className="font-bold">$75</span> yearly fee OR
-              <span className="font-bold"> $45</span> quarterly fee (only offered
-              winter and spring quarters)
+              <span className="font-bold"> $45</span> quarterly fee (only
+              offered winter and spring quarters)
             </p>
           </div>
           <div className="flex items-start">
             <Image
               src={Target}
               alt="Bullet Point 2"
-              className="ml-5 mr-4 md:ml-6 md:mr-5 xl:mx-7 xl:mr-6 w-[2.5vh] object-cover"
+              className="mr-4 ml-5 w-4 object-cover md:mr-5 md:ml-6 lg:mx-7 lg:mr-6 lg:w-6 xl:w-8"
             />
-            <p className="text-archery-grey-400 xl:mt-1 xl:text-xl text-xs font-bold">
+            <p className="text-archery-grey-400 text-xs font-bold lg:text-base xl:mt-1 xl:text-xl">
               Covers
             </p>
           </div>
         </div>
-        <div className="ml-21   xl:ml-26 mt-4 flex w-7/10 md:w-5/8 flex-col gap-4 xl:gap-4 justify-self-center text-start">
+        <div className="mt-4 ml-21 flex w-7/10 flex-col gap-4 justify-self-center text-start md:w-5/8 lg:ml-26 xl:mt-8 xl:ml-25 xl:gap-8">
           {Feeinfo.map(({ title, descriptions }, index) => (
-            <p key={index} className="text-archery-grey-400 xl:text-xl text-xs">
+            <p
+              key={index}
+              className="text-archery-grey-400 text-xs lg:text-base xl:text-xl"
+            >
               <span className="underline">{title}</span>
               {descriptions}
             </p>
