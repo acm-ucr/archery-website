14b600f2a7c121b5857eb0afc050fac63dad4f72
Yilin Liu <yilinliu741@gmail.com>
2026-02-27 19:45:29 -0800

    Added past changes

--- src/components/join/gallery.tsx
+++ src/components/join/gallery.tsx
@@ -7,39 +7,41 @@ import { Feeinfo } from "@/data/feeinfo";
 
 const Gallery = () => {
   return (
-    <div className="mt-40 grid w-5/6 grid-cols-2 justify-self-center">
-      <div className="col-start-2 row-start-1 mb-6 flex w-5/8 flex-col items-center justify-end gap-4 justify-self-center text-center">
-        <p className="text-archery-grey-400 mb-6 text-5xl leading-14 font-bold">
+    <div className="gap-6 md:gap-0 xl:mt-40 flex flex-col items-center md:ml-8 justify-self-center md:grid md:w-5/6 md:grid-cols-2">
+      <div className="border-1 flex flex-col md:ml-8 items-center xl:gap-2 justify-self-center text-center md:col-start-2 md:row-start-1 w-3/4 md:w-2/3 xl:w-5/8">
+        <p className="text-archery-grey-400 text-2xl xl:mb-6 xl:text-4xl xl:leading-11 font-bold">
           Everything you need to know about joining!
         </p>
-        <Image src={Warning} alt="Warning" className="mt-8 object-cover" />
-        <p className="text-2xl text-red-700">members must be UCR students</p>
+        <div className="justify-center flex gap-1 mt-4 xl:mt-6">
+          <Image src={Warning} alt="Warning" className="w-1/16 xl:w-1/12 object-cover" />
+          <p className="text-xs xl:text-xl text-red-700">members must be UCR students</p>
+        </div>
       </div>
-      <div className="row-span-2 row-start-1">
+      <div className="border-1 md:row-span-2 md:row-start-1 place-item-center">
         <GalleryGrid />
       </div>
-      <div className="col-start-2 row-span-1 row-start-2 w-3/4 flex-col justify-self-center text-center">
-        <div className="my-8 flex items-center justify-center gap-4">
+      <div className="border-1 md:w-8/9 xl:w-3/4 flex-col items-center justify-self-center text-center md:col-start-2 md:row-span-1 md:row-start-2">
+        <div className="xl:my-8 md:my-4 flex items-center justify-center gap-2 xl:gap-4">
           <Image
             src={SalaryMan}
             alt="Membership Fee"
-            className="justify-self-center object-cover"
+            className="justify-self-center w-1/7 object-cover"
           />
-          <p className="text-archery-grey-400 text-5xl font-bold">
+          <p className="text-archery-grey-400 xl:text-4xl text-2xl font-bold">
             Membership Fee
           </p>
         </div>
-        <div className="flex w-7/8 flex-col gap-8 justify-self-end">
+        <div className="flex w-7/8 flex-col gap-2 xl:gap-4 justify-self-end">
           <div className="flex items-start">
             <Image
               src={Target}
               alt="Bullet Point 1"
-              className="mx-8 w-12 object-cover"
+              className="mx-4 xl:mx-6 w-1/16 xl:w-1/12 object-cover"
             />
-            <p className="text-archery-grey-400 mt-1 w-2/3 text-start text-2xl">
+            <p className="text-archery-grey-400 xl:mt-1 w-2/3 text-start text-xs xl:text-xl">
               {" "}
               <span className="font-bold">$75</span> yearly fee OR
-              <span className="font-bold">$45</span> quarterly fee (only offered
+              <span className="font-bold"> $45</span> quarterly fee (only offered
               winter and spring quarters)
             </p>
           </div>
@@ -47,16 +49,16 @@ const Gallery = () => {
             <Image
               src={Target}
               alt="Bullet Point 2"
-              className="mx-8 w-12 object-cover"
+              className="mx-4 xl:mx-6 w-1/16 xl:w-1/12 object-cover"
             />
-            <p className="text-archery-grey-400 mt-1 text-2xl font-bold">
+            <p className="text-archery-grey-400 xl:mt-1 xl:text-xl text-xs font-bold">
               Covers
             </p>
           </div>
         </div>
-        <div className="mr-8 flex w-5/8 flex-col gap-6 justify-self-end text-start">
+        <div className="xl:mr-8 xl:mt-4 flex w-5/8 flex-col gap-2 xl:gap-4 justify-self-center xl:justify-self-end text-start">
           {Feeinfo.map(({ title, descriptions }, index) => (
-            <p key={index} className="text-archery-grey-400 text-2xl">
+            <p key={index} className="text-archery-grey-400 xl:text-xl text-xs">
               <span className="underline">{title}</span>
               {descriptions}
             </p>
