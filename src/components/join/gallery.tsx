a7f1b2c825d129912cd4002527c0db2c1f975803
Yilin Liu <yilinliu741@gmail.com>
2026-02-27 20:34:43 -0800

    Added margin for header and adjusted warning icon

--- src/components/join/gallery.tsx
+++ src/components/join/gallery.tsx
@@ -7,17 +7,17 @@ import { Feeinfo } from "@/data/feeinfo";
 
 const Gallery = () => {
   return (
-    <div className="gap-6 md:gap-0 xl:mt-40 flex flex-col items-center md:ml-8 justify-self-center md:grid w-5/6 md:grid-cols-2">
-      <div className="border-1 flex flex-col md:ml-8 items-center xl:gap-2 justify-self-center text-center md:col-start-2 md:row-start-1 md:w-2/3 xl:w-5/8">
+    <div className="gap-6 md:gap-0 mt-8 xl:mt-40 flex flex-col items-center md:ml-8 justify-self-center md:grid w-5/6 md:grid-cols-2">
+      <div className="border-1 flex flex-col md:ml-8 items-center xl:gap-2 text-center md:col-start-2 md:row-start-1 md:w-2/3 xl:w-5/8">
         <p className="text-archery-grey-400 text-xl xl:mb-6 xl:text-4xl xl:leading-11 font-bold">
           Everything you need to know about joining!
         </p>
-        <div className="justify-center flex gap-1 mt-4 xl:mt-6">
-          <Image src={Warning} alt="Warning" className="w-1/16 xl:w-1/12 object-cover" />
+        <div className="flex gap-1 mt-4 xl:mt-6">
+          <div className="w-[2.5vh] xl:w-1/12"><Image src={Warning} alt="Warning" className="object-cover" /></div>
           <p className="text-sm xl:text-xl text-red-700">members must be UCR students</p>
         </div>
       </div>
-      <div className="border-1 md:row-span-2 md:row-start-1 place-item-center">
+      <div className="border-1 md:row-span-2 md:row-start-1 ">
         <GalleryGrid />
       </div>
       <div className="border-1 md:w-8/9 xl:w-3/4 flex-col items-center justify-self-center text-center md:col-start-2 md:row-span-1 md:row-start-2">
@@ -56,7 +56,7 @@ const Gallery = () => {
             </p>
           </div>
         </div>
-        <div className="ml-16 xl:mt-4 flex w-5/8 flex-col gap-4 xl:gap-4 justify-self-center text-start">
+        <div className="ml-18 xl:mt-4 flex w-5/8 flex-col gap-4 xl:gap-4 justify-self-center text-start">
           {Feeinfo.map(({ title, descriptions }, index) => (
             <p key={index} className="text-archery-grey-400 xl:text-xl text-sm">
               <span className="underline">{title}</span>
