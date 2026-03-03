3fa7c138d726c9d1b42354612f802d65c08f3883
Yubin Zhen <yubinzhen020105@gmail.com>
2026-03-02 19:07:56 -0800

    addressed commments

--- src/components/infocard.tsx
+++ src/components/infocard.tsx
@@ -14,16 +14,14 @@ const InfoCard: React.FC<InfoProps> = ({
   description,
 }) => {
   return (
-    <div className="inline-flex h-[100px] w-[400px] items-center rounded-[27px] border border-[#0A3386] bg-white pr-8 pl-4">
-      {/* Icon */}
-      <div className="mr-3 flex h-12 w-12 items-center justify-center rounded-full">
-        <Icon className="text-3xl text-[#0A3386]" aria-hidden />
+    <div className="border-archery-blue-500 flex w-sm items-center rounded-2xl border py-5 pr-8 pl-1">
+      <div className="mr-3 flex h-12 w-12 items-center justify-center">
+        <Icon className="text-archery-blue-500 text-3xl" />
       </div>
 
-      {/* Text */}
-      <div className="flex flex-col justify-center">
-        <span className="text-lg font-semibold text-[#4C4C4C]">{title}</span>
-        <span className="text-sm text-[#4C4C4C]">{description}</span>
+      <div className="text-archery-grey-200 flex flex-col justify-center">
+        <div className="text-lg font-semibold">{title}</div>
+        <div className="text-sm">{description}</div>
       </div>
     </div>
   );
