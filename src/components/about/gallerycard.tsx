b4f34b5b9b43c4ff4247198d0d5419c9e87bfc3a
Tarunvir <gtarunvir@gmail.com>
2026-03-04 22:34:36 -0800

    quick size fix

--- src/components/about/gallerycard.tsx
+++ src/components/about/gallerycard.tsx
@@ -10,7 +10,7 @@ const GalleryCard = ({ title, image }: GalleryCardProps) => {
     <div className="relative aspect-[16/9] w-full overflow-hidden rounded-3xl md:aspect-[10/9] lg:aspect-[21/17]">
       <Image src={image} alt={title} fill className="object-cover" />
       <div className="bg-archery-grey-200/55 absolute inset-0 flex items-center justify-center shadow-lg">
-        <p className="font-archery-kaushan text-center text-3xl whitespace-pre-line text-white sm:text-4xl md:text-5xl lg:text-6xl">
+        <p className="font-archery-kaushan text-center text-5xl whitespace-pre-line text-white sm:text-6xl md:text-5xl lg:text-6xl">
           {title}
         </p>
       </div>
