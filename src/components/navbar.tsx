d55638b93fe473cb3631053f182de800cde1cb45
Yilin Liu <yilinliu741@gmail.com>
2026-03-04 23:20:23 -0800

    Adjusted responsiveness of gaps and size of the words and icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,12 +12,17 @@ const Navbar = () => {
   const closeMenu = () => setIsOpen(false);
   return (
     <div className="flex flex-col shadow-md">
-      <div className="flex w-full items-center justify-between ">
+      <div className="flex w-full items-center justify-between">
         <Link href="/" className="mt-2 ml-2">
-          <Image src={NavbarLogo} onClick={closeMenu} alt="NavbarLogo" className="w-1/2 lg:w-1/3" />
+          <Image
+            src={NavbarLogo}
+            onClick={closeMenu}
+            alt="NavbarLogo"
+            className="w-1/2 xl:w-1/3"
+          />
         </Link>
 
-        <div className="hidden md:flex items-center gap-8 p-10 text-2xl">
+        <div className="hidden items-center gap-4 p-4 text-base md:flex lg:gap-6 lg:p-7 lg:text-xl xl:gap-8 xl:p-10 xl:text-2xl">
           {navbarLinks.map(({ link, name }, index) => (
             <Link
               key={index}
@@ -29,29 +34,31 @@ const Navbar = () => {
           ))}
         </div>
 
-        <div className="md:hidden p-4">
-
-          <MenuIcon size={18} onClick={handleClick} className="text-archery-blue-600 "/>
-          
+        <div className="my-2 mr-4 md:hidden">
+          <MenuIcon
+            size={22}
+            onClick={handleClick}
+            className="text-archery-blue-600"
+          />
         </div>
+      </div>
 
-    </div>
-
-<div className={`overflow-hidden md:hidden flex flex-col border-t-1 items-end border-black/30 ${isOpen ? "h-auto" : "h-0 hidden"}`}>
-    <div className="w-max flex flex-col">
-      {navbarLinks.map(({ link, name }, index) => (
-        <Link
-          key={index}
-          href={link}
-          onClick={closeMenu}
-          className={`last:bg-archery-blue-600 text-sm last:mx-2 last:mb-4  px-4 mt-2 text-center text-black last:rounded-full last:text-white`}
-        >
-          {name}
-        </Link>
-      ))}
-    </div>
-    </div>
-
+      <div
+        className={`flex flex-col items-end overflow-hidden border-t-1 border-black/30 text-sm md:hidden ${isOpen ? "h-auto" : "hidden h-0"}`}
+      >
+        <div className="flex w-max flex-col">
+          {navbarLinks.map(({ link, name }, index) => (
+            <Link
+              key={index}
+              href={link}
+              onClick={closeMenu}
+              className={`last:bg-archery-blue-600 mt-2 px-4 text-center text-black last:mx-2 last:mb-4 last:rounded-full last:text-white`}
+            >
+              {name}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
