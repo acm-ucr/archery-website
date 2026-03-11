b2486be0ba9c2ed63fff8f6fc66db92e84746647
Yilin Liu <yilinliu741@gmail.com>
2026-03-10 22:55:18 -0700

    Added animation for the text section

--- src/components/join/galleryGrid.tsx
+++ src/components/join/galleryGrid.tsx
@@ -6,7 +6,7 @@ import bottomLeft from "@/public/gallery_bottomleft.webp";
 import topRight from "@/public/gallery_topright.webp";
 import midRight from "@/public/gallery_midright.webp";
 import bottomRight from "@/public/gallery_bottomright.webp";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 
 interface imageInfo {
   imageL: StaticImageData;
@@ -32,21 +32,28 @@ const GalleryGrid = () => {
   return (
     <div className="relative flex flex-col items-center gap-2">
       {ImageProp.map(({ imageL, imageR }, index) => (
-        <motion.div key={index} initial={{opacity:0, y:20}} transition={{duration:0.6, delay: index*0.15}} whileInView={{opacity:1, y:0}} viewport={{once : true}} className="flex gap-2">
-            <div className="relative">
-              <Image
-                src={imageL}
-                alt="Picture of Archery Club Members"
-                className="object-cover"
-              />
-            </div>
-            <div className="relative">
-              <Image
-                src={imageR}
-                alt="Picture of Archery Club Members"
-                className="object-cover"
-              />
-            </div>
+        <motion.div
+          key={index}
+          initial={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.6, delay: index * 0.1 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          className="flex gap-2"
+        >
+          <div className="relative">
+            <Image
+              src={imageL}
+              alt="Picture of Archery Club Members"
+              className="object-cover"
+            />
+          </div>
+          <div className="relative">
+            <Image
+              src={imageR}
+              alt="Picture of Archery Club Members"
+              className="object-cover"
+            />
+          </div>
         </motion.div>
       ))}
     </div>
