96f5e38420c9e58a7a2b67bdf7cbb9984b586919
Yilin Liu <yilinliu741@gmail.com>
2026-03-13 17:59:05 -0700

    added responsiveness for md and lg

--- src/components/home/getInvolved.tsx
+++ src/components/home/getInvolved.tsx
@@ -4,14 +4,14 @@ import YellowBow from "@/public/home-yellow-bow.svg";
 
 const GetInvolved = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-7 p-8">
-      <span className="text-center text-6xl font-extrabold text-black">
+    <div className="flex flex-col items-center justify-center md:gap-5 lg:gap-6 xl:gap-7 md:p-6 lg:p-7 xl:p-8">
+      <span className="text-center md:text-5xl xl:text-6xl font-extrabold text-black">
         Get Involved
       </span>
-      <span className="to-archery-yellow-300 from-archery-blue-400 bg-gradient-to-r from-20% to-70% bg-clip-text text-2xl font-bold text-transparent">
+      <span className="to-archery-yellow-300 from-archery-blue-400 text-center bg-gradient-to-r from-20% to-70% bg-clip-text text-xs w-3/4  md:text-xl xl:text-2xl font-bold text-transparent">
         Take a shot at something new -- Join Archery @ UCR!!
       </span>
-      <div className="flex flex-row items-center justify-center gap-x-20 p-8">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-y-4  md:gap-x-8 lg:gap-x-10 md:p-4 xl:gap-x-20 xl:p-8">
         <InvolvedCard
           borderColor="border-archery-blue-300"
           bgColor="bg-archery-blue-300/10"
--- src/components/home/involvedCard.tsx
+++ src/components/home/involvedCard.tsx
@@ -19,12 +19,12 @@ const InvolvedCard = ({
 }: InvolvedProps) => {
   return (
     <div
-      className={`flex h-[35vh] w-[45vw] items-center justify-center rounded-2xl border-1 border-solid ${bgColor} ${borderColor}`}
+      className={`flex h-[15vh] w-[80vw] md:h-[20vh] md:w-[43vw] xl:w-[45vw] lg:h-[25vh] xl:h-[35vh]  items-center justify-center rounded-2xl border-1 border-solid ${bgColor} ${borderColor}`}
     >
       <div>
-        <div className="mb-8 flex items-center gap-x-8">
+        <div className="md:mb-3 lg:mb-5 xl:mb-8 flex items-center md:gap-x-4  lg:gap-x-6 xl:gap-x-8">
           <div
-            className={`flex h-[8vh] w-[4vw] items-center justify-center rounded-2xl ${iconBG}`}
+            className={`flex h-[4vh] w-[4vh] md:h-[5vh] md:w-[5vw] lg:h-[6vh]  xl:h-[8vh] xl:w-[4vw] items-center justify-center rounded-xl xl:rounded-2xl ${iconBG}`}
           >
             <Image
               src={icon}
@@ -32,12 +32,12 @@ const InvolvedCard = ({
               className="h-3/4 w-3/4 object-contain"
             />
           </div>
-          <p className="text-archery-grey-300 text-center text-4xl font-bold">
+          <p className="text-archery-grey-300 text-center xl:text-4xl md:text-lg lg:text-2xl font-bold">
             {title}
           </p>
         </div>
-        <div className="h-[10vh] w-[40vw]">
-          <p className="text-archery-grey-200 text-left text-xl">
+        <div className=" h-[6vh] w-[60vw] md:h-[8vh] xl:h-[10vh] md:w-[35vw] xl:w-[40vw]">
+          <p className="text-archery-grey-200 text-left xl:text-xl md:text-sm lg:text-base text-xs">
             {description}
           </p>
         </div>
