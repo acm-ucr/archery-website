257fc9d7044d600f8390d25ee58001f9a91cd35a
j1munoz <jimmymunoz397@gmail.com>
2026-03-25 19:05:26 -0700

    Fixes

--- src/components/about/openPractices.tsx
+++ src/components/about/openPractices.tsx
@@ -6,11 +6,11 @@ import { LiaMapMarkerAltSolid } from "react-icons/lia";
 
 const OpenPractices = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <div className="relative p-15">
         <Subheader name="Open Practices" />
       </div>
-      <div className="flex flex-row justify-center gap-6 px-6 py-6 sm:gap-3 md:gap-5">
+      <div className="flex flex-col justify-center gap-6 px-6 py-6 sm:gap-3 md:flex-row md:gap-5">
         <InfoCard title="Time" description="Every Friday 2-5:45pm" />
         <InfoCard
           icon={LiaMapMarkerAltSolid}
@@ -19,34 +19,15 @@ const OpenPractices = () => {
         />
       </div>
 
-      <div className="flex flex-col items-center gap-2 px-10 sm:flex-row sm:items-end sm:justify-center lg:px-25">
-        <div className="relative aspect-[3/4] h-[420px] w-full sm:flex-1">
-          <Image
-            src={OpenPracticePics.left}
-            alt="Open Practice Left"
-            width={400}
-            height={533}
-            className="h-full w-full object-cover"
-          />
+      <div className="flex items-center justify-center gap-4 md:w-5/6 lg:w-6/7 xl:w-full">
+        <div className="hidden h-[386px] w-[290px] md:block">
+          <Image src={OpenPracticePics.left} alt="Open Practice Left" />
         </div>
-
-        <div className="relative aspect-[3/4] h-[420px] w-full sm:flex-[1.4]">
-          <Image
-            src={OpenPracticePics.mid}
-            alt="Our Coaches"
-            width={600}
-            height={600}
-            className="h-full w-full object-cover"
-          />
+        <div className="w-3/4 md:h-[386px] md:w-[514px]">
+          <Image src={OpenPracticePics.mid} alt="Our Coaches" />
         </div>
-        <div className="relative aspect-[3/4] h-[420px] w-full sm:flex-1">
-          <Image
-            src={OpenPracticePics.right}
-            alt="Open Practices"
-            width={400}
-            height={533}
-            className="h-full w-full object-cover"
-          />
+        <div className="hidden h-[386px] w-[290px] md:block">
+          <Image src={OpenPracticePics.right} alt="Open Practice Right" />
         </div>
       </div>
     </div>
