6a3fae43e5b48ec8f9fe677620fbbe3a40373aa2
j1munoz <jimmymunoz397@gmail.com>
2026-03-25 20:23:23 -0700

    Fixes

--- src/components/home/ourMission.tsx
+++ src/components/home/ourMission.tsx
@@ -6,15 +6,14 @@ interface MissionProps {
 const Mission = ({ title_our, title_mission }: MissionProps) => {
   return (
     <div className="flex flex-col items-center justify-center gap-10 p-8">
-      <div className="flex flex-row gap-6 text-center text-6xl font-extrabold">
+      <div className="flex flex-row gap-2 text-center text-4xl font-extrabold md:gap-6 md:text-6xl">
         <span className="text-black">{title_our}</span>
-
         <span className="from-archery-yellow-300 to-archery-blue-400 bg-gradient-to-r from-25% to-75% bg-clip-text text-transparent">
           {title_mission}
         </span>
       </div>
 
-      <div className="text-archery-grey-200 max-w-4xl text-center text-xl">
+      <div className="text-archery-grey-200 max-w-4xl text-center text-base md:text-xl">
         <p>
           <span className="text-archery-blue-700 font-bold">
             UCR Archery Club
--- src/components/join/faq.tsx
+++ src/components/join/faq.tsx
@@ -9,13 +9,13 @@ import { FAQdata } from "@/data/faq";
 
 const FAQ = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center pb-10">
       <div className="w-[40vw] py-9">
-        <p className="text-center text-6xl font-extrabold text-black">
+        <p className="text-center text-3xl font-extrabold text-black md:text-6xl">
           Frequently Asked Questions
         </p>
       </div>
-      <div className="w-[50vw] text-left">
+      <div className="w-5/6 text-left md:w-[50vw]">
         <Accordion
           type="single"
           collapsible
@@ -25,12 +25,14 @@ const FAQ = () => {
           {FAQdata.map(({ question, answer }, index) => (
             <AccordionItem key={index} value={`item-${index}`}>
               <AccordionTrigger>
-                <p className="text-archery-grey-400 px-3 font-sans text-xl font-semibold">
+                <p className="text-archery-grey-400 px-3 font-sans text-lg font-semibold md:text-xl">
                   {question}
                 </p>
               </AccordionTrigger>
               <AccordionContent>
-                <p className="w-[40vw] px-3 text-base text-black">{answer}</p>
+                <p className="w-[40vw] px-3 text-sm text-black md:text-base">
+                  {answer}
+                </p>
               </AccordionContent>
             </AccordionItem>
           ))}
